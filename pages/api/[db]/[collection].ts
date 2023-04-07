c63d5d27727292894839102223212a01d233d821
OsoriAndOmori <skywhite15@korea.ac.kr>
2023-04-08 01:07:17 +0900

    API URL Design 변경

--- pages/api/[db]/[collection].ts
+++ pages/api/[db]/[collection].ts
@@ -1,14 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
-import clientPromise from "../../../../../lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
+
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
     return clientPromise
         .then((client) => client.db(req.query['db'] as string).collection(req.query['collection'] as string))
-        .then(collection => collection.findOne({_id: req.query['id'] as string}))
-        .then(value => {
-            console.log(value);
-            res.status(200).json(value)
-        })
+        .then(collection => collection.find({}, {limit: 10}).toArray())
+        .then(results => res.status(200).json(results))
         .catch((err) => console.log(err));
 }
--- pages/api/[db]/[collection]/[id].ts
+++ pages/api/[db]/[collection]/[id].ts
@@ -2,11 +2,13 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import clientPromise from "../../../../lib/mongodb";
 
-
 export default (req: NextApiRequest, res: NextApiResponse) => {
     return clientPromise
         .then((client) => client.db(req.query['db'] as string).collection(req.query['collection'] as string))
-        .then(collection => collection.find({}, {limit: 10}).toArray())
-        .then(results => res.status(200).json(results))
+        .then(collection => collection.findOne({_id: req.query['id'] as string}))
+        .then(value => {
+            console.log(value);
+            res.status(200).json(value)
+        })
         .catch((err) => console.log(err));
 }
