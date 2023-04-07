66cebdd66f21c63c2d0fdad2f05a3a5a4ae7eb5a
OsoriAndOmori <skywhite15@korea.ac.kr>
2023-04-08 01:49:25 +0900

    naver map api 붙이기

---
+++ pages/api/map/gohome.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import {NextApiRequest, NextApiResponse} from "next";
+
+const X_NCP_APIGW_API_KEY_ID = process.env.X_NCP_APIGW_API_KEY_ID;
+const X_NCP_APIGW_API_KEY = process.env.X_NCP_APIGW_API_KEY
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    const config = {
+        headers: {
+            'X-NCP-APIGW-API-KEY-ID': X_NCP_APIGW_API_KEY_ID,
+            'X-NCP-APIGW-API-KEY': X_NCP_APIGW_API_KEY
+        },
+        params: {
+            'start': req.query['start'],
+            'goal': req.query['goal']
+        }
+    };
+
+    return axios.get('https://naveropenapi.apigw.ntruss.com/map-direction-15/v1/driving', config)
+        .then(response => {
+            console.log(response.data);
+            res.status(200).json(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
