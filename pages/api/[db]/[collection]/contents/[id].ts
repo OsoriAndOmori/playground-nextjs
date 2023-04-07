9adf6dcf80cd92d94e58d8bb10ec3346347bebae
OsoriAndOmori <skywhite15@korea.ac.kr>
2023-04-08 01:02:50 +0900

    mongo 설정 추가

---
+++ pages/api/[db]/[collection]/contents/[id].ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next'
+import clientPromise from "../../../../../lib/mongodb";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    return clientPromise
+        .then((client) => client.db(req.query['db'] as string).collection(req.query['collection'] as string))
+        .then(collection => collection.findOne({_id: req.query['id'] as string}))
+        .then(value => {
+            console.log(value);
+            res.status(200).json(value)
+        })
+        .catch((err) => console.log(err));
+}
