f0745de937795e27a1f3ee56aacde7c0bcea2c89
OsoriAndOmori <skywhite15@korea.ac.kr>
2023-04-10 14:59:51 +0900

    redirect url 환경 변수로 변경

--- pages/api/kakao/code.ts
+++ pages/api/kakao/code.ts
@@ -1,11 +1,13 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
 const client_id = process.env.KAKAO_APP_CLIEND_ID || "";
+const redirectUrl = process.env.KAKAO_APP_REDIRECT_URL  || "";
+
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
     const params = {
         'client_id': client_id,
-        'redirect_uri': "http://localhost:3000/api/kakao/login",
+        'redirect_uri': redirectUrl,
         'response_type': "code"
     };
     const url = "https://kauth.kakao.com/oauth/authorize"
