c61676a6b6692621ca88703198dcff3a6ad5ea1d
OsoriAndOmori <skywhite15@korea.ac.kr>
2023-04-28 22:32:16 +0900

    slack 으로 집가라고 신호 주기

--- packages/api/pages/api/map/gohome.ts
+++ packages/api/pages/api/map/gohome.ts
@@ -1,10 +1,18 @@
 import axios from 'axios';
 import {NextApiRequest, NextApiResponse} from "next";
+import {WebClient} from '@slack/web-api'
 
+// An access token (from your Slack app or custom integration - xoxp, xoxb)
+const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN;
 const X_NCP_APIGW_API_KEY_ID = process.env.X_NCP_APIGW_API_KEY_ID;
 const X_NCP_APIGW_API_KEY = process.env.X_NCP_APIGW_API_KEY
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const web = new WebClient(SLACK_API_TOKEN);
+
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+const conversationId = 'D053U393B8D';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     const config = {
         headers: {
             'X-NCP-APIGW-API-KEY-ID': X_NCP_APIGW_API_KEY_ID,
@@ -15,15 +23,16 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
             'goal': req.query['goal']
         }
     };
-
-    return axios.get('https://naveropenapi.apigw.ntruss.com/map-direction-15/v1/driving', config)
-        .then(response => {
-            console.log(response.data);
-            res.status(200).json(response.data);
-            return response.data;
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    const response = await axios.get('https://naveropenapi.apigw.ntruss.com/map-direction-15/v1/driving', config);
+    const summary = response.data.route.traoptimal[0].summary;
+    const duration = summary.duration;
+    await send(`지금 당장 집으로 출발하면 일산 Sphere 에서 수명산 파크까지 ${Math.floor(duration / 1000 / 60)}분 걸립니다.`);
+    res.status(200).json(summary);
+    return summary;
 }
+
+const send = async (message) => {
+    // See: https://api.slack.com/methods/chat.postMessage
+    await web.chat.postMessage({ username: '김나린김나율', channel: conversationId, text: message });
+};
 
